067462a98befce27bf932c0bd3444c4c776e4341
Fradique Lee <fradique.lee@conduent.com>
2020-03-18 18:42:47 -0600

    bar config

--- src/app/bar-chart/bar-chart.component.ts
+++ src/app/bar-chart/bar-chart.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Coronavirus } from './../model/coronavirus';
+import { Subscription } from 'rxjs';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
 import { Label } from 'ng2-charts';
 
@@ -7,24 +10,33 @@ import { Label } from 'ng2-charts';
   templateUrl: './bar-chart.component.html',
   styleUrls: ['./bar-chart.component.scss']
 })
-export class BarChartComponent implements OnInit {
-
+export class BarChartComponent implements OnInit, OnDestroy {
+  sub: Subscription;
   public barChartOptions: ChartOptions = {
     responsive: true,
+    scales: { yAxes: [{ ticks: { min: 0, stepSize: 1 } }] },
+    animation: { duration: 0 }
   };
-  public barChartLabels: Label[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public barChartLabels: Label[] = [];
   public barChartType: ChartType = 'bar';
-  public barChartLegend = true;
+  public barChartLegend = false;
   public barChartPlugins = [];
 
-  public barChartData: ChartDataSets[] = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
-  ];
+  public barChartData: ChartDataSets[] = [];
 
-  constructor() { }
+  constructor(private db: AngularFirestore) {}
 
-  ngOnInit() {
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
   }
 
+  ngOnInit() {
+    this.sub = this.db
+      .collection<Coronavirus>('coronavirus')
+      .valueChanges()
+      .subscribe(films => {
+        this.barChartLabels = films.map(f => f.pais);
+        this.barChartData = [{ data: films.map(f => f.infectados) }];
+      });
+  }
 }
